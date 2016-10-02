f5cd0a70fb6ce626c82928d6b68d49e956bf1043
Tristan Simpson <tristansimpson@tristans-macbook-air.fritz.box>
2016-10-02 15:37:18 +1300

    Initial Commit

---
+++ gitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitDemo
+//
+//  Created by Tristan Simpson on 2/10/16.
+//  Copyright © 2016 Tristan Simpson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
